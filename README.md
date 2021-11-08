8f5ec33f743d16f705581d7b4fb6800d0cb8ad00
hà huy hùng <83862705+ZeroRyo@users.noreply.github.com>
2021-11-08 10:04:23 +0700

    up

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learn_English_Today
+
+
+
+## Screenshots
+| ![Water summary](screenshots/info.jpg) | ![Progress](screenshots/intro1.jpg) | ![Settings](screenshots/list.jpg) | ![Alarm](screenshots/navbar.jpg) |
+|----------|:-------------:|:-------------:|:-------------:|
---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,12 @@
+import 'package:Learn_English_Today/models/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://scontent.fdad3-2.fna.fbcdn.net/v/t39.30808-6/244465416_634909057477511_3418082064943834356_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lt7RudD_KZAAX_8ITcE&_nc_ht=scontent.fdad3-2.fna&oh=af54f8e0caf8bfe420b1a5fbf8130f4b&oe=618E0CB1',
+    name: 'Ha Huy Hung',
+    email: 'hhhungdesigner@gmail.com',
+    about:'📘 Currently studying on TDTU                                        💻 Working on ... Technology                    ✍️ Making some projects using JavaScript, PHP,...                                         📂 Node.js, React.js, TensorFlow and Laravel',
+    isDarkMode: false,
+  );
+}
